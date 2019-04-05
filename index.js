506d6813ac786f4ebbaf97b04d360962d1fe31e0
Sathish Babu <sathishbabur@hotmail.com>
2019-04-05 11:27:57 +0530

    Removed interval update

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  
 
   console.log("websocket connection open")
 
